fe81bf9a3900cbdd986398fc222c73348ba5d38b
tomashm01 <tomashidalgomartin@gmail.com>
2022-02-02 21:42:21 +0100

    feat: version consola funciona

--- MasterMind.js
+++ MasterMind.js
@@ -3,8 +3,6 @@
     let MasterMind=function(){
 
         const arrayColores=["red","blue","green","yellow","orange","purple","pink","black","white"];
-        const NUM_COLORS=arrayColores.length;
-        const NUM_COLUMNS=4;
         let linea;
 
         const init=function(){
@@ -17,60 +15,38 @@
             console.log(linea);
         }
 
-        const contarBlancos=function(array){
-            let contador=0;
-            for(let i=0;i<array.length;i++){
-                if(array[i]=="white"){
-                    contador++;
-                }
-            }
-            return contador;
-
+        const contarBlancos = function(combinacionUsuario, combinacionGanadora) {
+            let bolasBlancas = 0;
+    
+            combinacionUsuario.forEach(function(elemento) {
+                let indice = combinacionGanadora.indexOf(elemento);
+                if (indice != -1) bolasBlancas++;
+            });
+    
+            return bolasBlancas;
         }
         
-        const contarNegros=function(array){
-            let contador=0;
-            for(let i=0;i<array.length;i++){
-                if(array[i]=="black"){
-                    contador++;
+        const contarNegros = function(combinacionUsuario, combinacionGanadora) {
+            let bolasNegras = 0;
+    
+            combinacionUsuario.forEach(function(elemento, indice) {
+                if (elemento == combinacionGanadora[indice]){
+                    bolasNegras++;
+                    combinacionUsuario[indice] = null;
                 }
-            }
-            return contador;
-
-        } 
-
-        const comprobarGanar=function(intento,intento2){
-            let contador=0;
-            for(let i=0;i<intento.length;i++){
-                if(intento[i]==intento2[i]){
-                    contador++;
-                }
-            }
-            if(contador==4) return true;
-            return false;
+            });
+    
+            return bolasNegras;
         }
-
-        const escribirNegroBlanco=function(intento,intento2){
-            let array=[];
-            for(let i=0;i<intento.length;i++){
-                if(intento[i]==intento2[i]){
-                    array.push("black");
-                }else{
-                    array.push("white");
-                }
-            }
-            return array;
-        }
-
+    
         const comprobarConcidencia= function(intento){
             
-            let [...intento2]=intento;
+            let copiaLinea=linea.slice();
+            let combinacionUsuario=intento.slice();
 
             return {
-                intento:escribirNegroBlanco(intento,intento2),
-                numBlancos: contarBlancos(intento),
-                numNegros: contarNegros(intento),
-                ganado: comprobarGanar(intento,intento2)
+                bolasNegras: contarNegros(combinacionUsuario,copiaLinea),
+                bolasBlancas: contarBlancos(combinacionUsuario,copiaLinea)
             };
             
         }
